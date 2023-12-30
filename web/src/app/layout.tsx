cd652def7e51efbb6b5ef63a5e7028d35aa94abb
henrique1501 <vicsam7716@gmail.com>
2023-12-30 13:44:48 +0000

    setup: web finished

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+
+export const metadata: Metadata = {
+  title: 'Podcastr',
+  description: 'Developers podcast app',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
